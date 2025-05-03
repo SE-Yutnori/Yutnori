3d5de01b29c52216330bf655e33fcee7a664bf78
BAE GEUNWOO <92969924+GWB21@users.noreply.github.com>
2025-05-03 17:03:44 +0900

    BoardNode 설정

--- src/model/BoardBuilder.java
+++ src/model/BoardBuilder.java
@@ -13,6 +13,20 @@ public class BoardBuilder {
 
         List<BoardNode> nodes = new ArrayList<>();
 
+        //Center노드의 X,Y 좌표 설정
+        float centerX = 2.5f;
+        float centerY = 2.5f;
+
+        //Center노드 생성자로 생성
+        BoardNode center = new BoardNode("Center", centerX, centerY, sides);
+        // 노드 리스트에 추가
+        nodes.add(center);
+        // 리스트 생성
+        // 외곽 변 노드 리스트
+        BoardNode[][] edgeNodes = new BoardNode[sides][6];
+        //Center로 향하는 변에 위치한 노드 리스트 2개 생성
+        BoardNode[] toCenterPath1 = new BoardNode[sides];
+        BoardNode[] toCenterPath2 = new BoardNode[sides];
         return nodes;
     }
 }
