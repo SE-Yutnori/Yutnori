0ccdf98c083a1e7656d7e2c937900f464dd9ad30
BAE GEUNWOO <92969924+GWB21@users.noreply.github.com>
2025-05-03 18:17:45 +0900

    creating outside line nodes

--- src/model/BoardBuilder.java
+++ src/model/BoardBuilder.java
@@ -27,6 +27,33 @@ public class BoardBuilder {
         //Center로 향하는 변에 위치한 노드 리스트 2개 생성
         BoardNode[] toCenterPath1 = new BoardNode[sides];
         BoardNode[] toCenterPath2 = new BoardNode[sides];
+
+        // 외곽 노드 만들기
+        for (int i = 0; i < sides; i++) {
+            double angle1 = 2 * Math.PI * i / sides - Math.PI / 2;
+            double angle2 = 2 * Math.PI * (i + 1) / sides - Math.PI / 2;
+
+            float x1 = (float)(Math.cos(angle1) * radius + centerX);
+            float y1 = (float)(Math.sin(angle1) * radius + centerY);
+            float x2 = (float)(Math.cos(angle2) * radius + centerX);
+            float y2 = (float)(Math.sin(angle2) * radius + centerY);
+
+            for (int j = 0; j < 6; j++) {
+                float t = j / 5f;
+                float x = x1 + (x2 - x1) * t;
+                float y = y1 + (y2 - y1) * t;
+
+                if (j == 0 && i > 0) {
+                    // 이전 면의 마지막 노드와 현재 면의 첫 노드가 동일
+                    edgeNodes[i][j] = edgeNodes[i - 1][5];
+                } else {
+                    BoardNode node = new BoardNode("Edge" + i + "-" + j, x, y, sides);
+                    edgeNodes[i][j] = node;
+                    nodes.add(node);
+                }
+            }
+        }
+
         return nodes;
     }
 }
