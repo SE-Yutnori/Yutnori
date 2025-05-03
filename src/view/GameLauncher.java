b0ea34a10ff519c177e96ac7f00e280e583e354a
BAE GEUNWOO <92969924+GWB21@users.noreply.github.com>
2025-05-03 15:52:32 +0900

    GameLauncher annotation

--- src/view/GameLauncher.java
+++ src/view/GameLauncher.java
@@ -1,4 +1,15 @@
 package view;
 
+//게임을 시작하는 class
 public class GameLauncher {
+    /**
+     * 게임에 필요한 정보 수집 이후 플레이
+     * 1. 보드 각형
+     * 2. 테스트 모드 여부
+     * 3. 플레이어 수
+     * 4. 플레이어가 사용할 말 개수
+     */
+    public void start() {
+    }
 }
+
