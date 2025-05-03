513ae7a8a9f26eff3c8d32d2359006fd27c4b575
BAE GEUNWOO <92969924+GWB21@users.noreply.github.com>
2025-05-03 18:45:34 +0900

    getPlayers method implemented

--- src/view/GameLauncher.java
+++ src/view/GameLauncher.java
@@ -2,9 +2,13 @@ package view;
 
 import model.BoardBuilder;
 import model.BoardNode;
+import model.Player;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //게임을 시작하는 class
 public class GameLauncher {
@@ -25,7 +29,8 @@ public class GameLauncher {
             // sides 이용하여 BoardBuilder의 buildCustomizingBoard 메서드를 불러와 BoardNode 리스트 생성
             List<BoardNode> board = BoardBuilder.buildCustomizingBoard(sides, 2f);
 
-
+            // 내부에서 getPlayerCount() 메서드를 내부에서 불러와 플레이어 정보(플레이어 수, 플레이어 이름, 말 수)를 입력받음
+            List<Player> players = getPlayers();
         });
     }
     // 사용자에게 n각형 커스터마이징을 입력받는 메서드
@@ -41,5 +46,46 @@ public class GameLauncher {
             }
         }
         return sides; //입력받은 n값을 반환.
+    }
+
+    //플레이어 정보를 입력 받는 메서드 (플레이어 수 , 플레이어 이름, 사용할 말의 갯수)
+    private List<Player> getPlayers() {
+        int numPlayers = getPlayerCount(); //사용자 수를 입력받는 메서드
+        int numTokens = getTokenCount(); //사용할 말의 갯수를 입력받는 메서드
+
+        List<Player> players = new ArrayList<>();
+        Set<String> usedNames = new HashSet<>();
+
+        for (int i = 1; i <= numPlayers; i++) {
+            String name = null;
+            while (true) {
+                name = JOptionPane.showInputDialog(null, "플레이어 " + i + "의 이름을 입력하세요:");
+                if (name == null) System.exit(0); // 예외처리 : 취소 혹은 창 닫기를 누를 시 종료.
+                name = name.trim(); //공백 이름 허용 안함
+                if (name.isEmpty()) { //예외처리 : 공백, 중복 -> 잘못된 입력은 무시하고 재입력하게끔
+                    JOptionPane.showMessageDialog(null, "이름은 필수입니다.");
+                } else if (usedNames.contains(name)) {
+                    JOptionPane.showMessageDialog(null, "중복 이름입니다.");
+                } else {
+                    usedNames.add(name);
+                    break;
+                }
+            }
+            // 플레이어 생성
+            players.add(new Player(name, numTokens));
+        }
+        return players;
+    }
+
+    // 플레이어 수를 입력받는 메서드 (2-4명)
+    private int getPlayerCount() {
+        int numPlayers = 0;
+        return numPlayers;
+    }
+
+    //사용할 말의 갯수를 입력 받는 메서드 (2-5명)
+    private int getTokenCount(){
+        int tokenCount = 0;
+        return tokenCount;
     }
 }
